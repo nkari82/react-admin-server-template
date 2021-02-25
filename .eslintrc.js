8422c09c1cd0b47264c54085d77d77d6ee416f4a
nkari82 <nkari82@gmail.com>
2021-02-25 12:30:05 +0900

    init

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/eslint-recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+  ],
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  env: {
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
+      arrowFunctions: true,
+    },
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  settings: {
+    react: {
+      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        paths: ['./src'],
+      },
+    },
+  },
+  ignorePatterns: ['dist/', 'node_modules/'],
+  rules: {
+    // Existing rules
+    'comma-dangle': 'off', // https://eslint.org/docs/rules/comma-dangle
+    'function-paren-newline': 'off', // https://eslint.org/docs/rules/function-paren-newline
+    'global-require': 'off', // https://eslint.org/docs/rules/global-require
+    'import/no-dynamic-require': 'off', // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-dynamic-require.md
+    'no-inner-declarations': 'off', // https://eslint.org/docs/rules/no-inner-declarations
+    // New rules
+    'class-methods-use-this': 'off',
+    'import/extensions': 'off',
+    'import/prefer-default-export': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+  },
+};
